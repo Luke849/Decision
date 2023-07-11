d3c9fd41e8fd71a0966cef63f22a466f021678ce
Luke849 <81688727+Luke849@users.noreply.github.com>
2023-07-11 23:17:35 +0200

    second

---
+++ script.js
@@ -0,0 +1,17 @@
+let ownDecisions = 0;
+let othersDecision =0;
+const btnOthers = document.getElementById('others');
+const btnOwn = document.getElementById('own');
+
+function getOthers() {
+    btnOthers.innerHTML = othersDecision;
+}
+function addOthers() {
+    othersDecision += 1;
+}
+
+function others() {
+    getOthers
+    addOthers
+    getOthers
+}
